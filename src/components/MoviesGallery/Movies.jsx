36cbbba4a7225e0745c09c4a7b60216cf92db53d
Maksym Lepikhov <Werzeragna@icloud.com>
2023-04-17 15:48:42 +0300

    Modal

--- src/components/MoviesGallery/Movies.jsx
+++ src/components/MoviesGallery/Movies.jsx
@@ -1,6 +1,6 @@
 import React, {Component} from "react";
 
-export const MoviesGallery = ({movies, onDelete}) => {
+export const MoviesGallery = ({movies, onDelete, openModal}) => {
     return (
         <ul>
             {movies.map(movie => (
@@ -8,6 +8,7 @@ export const MoviesGallery = ({movies, onDelete}) => {
                     <h2>{movie.title}</h2>
                     <p>Votes: {movie.vote_count}</p>
                     <button onClick={() => {onDelete(movie.id)}}>Delete</button>
+                    <button onClick={() => {openModal({src: movie.poster_path, alt: movie.title})}}>Show poster</button>
                 </li>
             ))}
         </ul>
